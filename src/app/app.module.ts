a2837c376920cc11bdf40811d0c59cba80ce2231
raulereno <raulereno@gmail.com>
2022-01-23 11:00:58 -0300

    componentes para portfolio agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PortadaComponent } from './componentes/portada/portada.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { ResumenComponent } from './componentes/resumen/resumen.component';
+import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PortadaComponent,
+    SobreMiComponent,
+    ResumenComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule
